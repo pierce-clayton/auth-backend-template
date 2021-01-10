592892cfb813f8dbe39371d8a16c966079a2cb80
Clayton Pierce <46853031+technicholy@users.noreply.github.com>
2021-01-10 13:49:31 -0800

    completes session_store initializer

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: '_auth_app', domain: 'http://localhost:3000'
+if Rails.env == 'production'
+  Rails.application.config.session_store :cookie_store, key: '_auth_app', domain: 'http://mydomain.blah:3000'
+else
+  Rails.application.config.session_store :cookie_store, key: '_auth_app'
+end
